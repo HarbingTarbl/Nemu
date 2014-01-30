5d0212e8bb17ef1e2c640ca193bcb5f77cf07557
HarbingTarbl <harbingtarbl@gmail.com>
2014-01-30 12:29:57 -0500

    Added NesCPU, nesdev info

--- src/6502.hpp
+++ src/6502.hpp
@@ -1,12 +1,26 @@
 #pragma once
 #ifndef _6502_H_PP
 #define _6502_H_PP
+#include <cstdint>
+
+class NesCPU
+{
+private:
 
 
+public:
+	uint8_t 
+		AccumReg,
+		XIndexReg,
+		YIndexReg,
+		StatusReg,
+		SPReg;
 
+	uint16_t 
+		PCReg;
 
+	uint8_t StackSpace;
 
-
-
+};
 
 #endif
