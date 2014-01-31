9a2eee918b3f5e2fd3066270f3e5ca3fb670847f
HarbingTarbl <harbingtarbl@gmail.com>
2014-01-30 19:03:34 -0500

    Removed MemoryMap, going more OO

--- src/6502.hpp
+++ src/6502.hpp
@@ -2,59 +2,31 @@
 #ifndef _6502_H_PP
 #define _6502_H_PP
 #include <cstdint>
+#include "OpCodes.hpp"
 
-template<int Size>
-class MemoryPage
+class CPUState
 {
-private:
-	uint8_t m_Data[Size];
-
 public:
-	inline void Write(uint8_t offset, uint8_t value)
+	uint8_t SP;
+	uint8_t IR;
+	uint16_t PC;
+	uint8_t X;
+	uint8_t Y;
+	uint8_t A;
+	union 
 	{
-		m_Data[offset] = value;
-	}
-
-	inline uint8_t Read(uint8_t offset)
-	{
-		return m_Data[offset];
-	}
-
-	inline uint8_t& operator[](int offset)
-	{
-		return m_Data[offset];
-	}
+		bool CarryFlag : 1;
+		bool ZeroFlag : 1;
+		bool InterruptFlag : 1;
+		bool DecimalFlag : 1;
+		bool BreakFlag : 1;
+		bool __UnusedFlag : 1;
+		bool OverflowFlag : 1;
+		bool SignFlag : 1;
+		uint8_t Status;
+	};
+	bool HasAsserted;
 };
 
-class NesCPU
-{
-private:
-
-
-public:
-	uint8_t 
-		AccumReg,
-		XIndexReg,
-		YIndexReg,
-		SPReg;
-
-	union
-	{
-		uint8_t Value;
-		struct
-		{
-			uint8_t Carry : 1;
-			uint8_t Zero : 1;
-			uint8_t Decimal : 1;
-			uint8_t InterruptDisable : 1;
-			uint8_t Breakpoint : 1;
-			uint8_t : 1;
-			uint8_t Overflow : 1;
-			uint8_t Sign : 1;
-		} Flags;
-	} StatusReg;
-
-	uint16_t PCReg;
-};
 
 #endif
